1566c153915aedd1a4fc46ef78d1a3ec95c66ea5
Prakyath Kolla <prakyathkolla98@gmail.com>
2024-08-25 00:00:32 -0400

    Updating files

--- next.config.js
+++ next.config.js
@@ -2,13 +2,25 @@ const webpack = require('webpack');
 require('dotenv').config();
 
 module.exports = {
-  webpack: config => {
+  webpack: (config, { isServer }) => {
     const env = Object.keys(process.env).reduce((acc, curr) => {
       acc[`process.env.${curr}`] = JSON.stringify(process.env[curr]);
       return acc;
     }, {});
 
     config.plugins.push(new webpack.DefinePlugin(env));
+
+    // Add the fallback for crypto
+    config.resolve.fallback = {
+      crypto: require.resolve("crypto-browserify")
+    };
+
+    // Add the openssl legacy provider for Node.js 17+ compatibility
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.NODE_OPTIONS': JSON.stringify('--openssl-legacy-provider')
+      })
+    );
 
     return config;
   }
